b05d49ca05df480b3137355156dc68ce9628ec65
PratikGamez <pratikgamer2065@gmail.com>
2026-01-13 15:10:51 +0545

    fadsfasd
    af

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
 {
 
     public Vector2 playerMoveDir {get; private set;}
+    public bool isInputJump {get; private set;}
 
     private PlayerControl inputActions;
 
@@ -28,7 +29,11 @@ public class InputManager : MonoBehaviour
     {
         if(callbackContext.performed)
         {
-         Debug.Log("Jumped");             
+            isInputJump = true;
+        }
+        if(callbackContext.canceled)
+        {
+            isInputJump = false;
         }
     }
 }
